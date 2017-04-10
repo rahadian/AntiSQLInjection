e4cedaa0ee76fd35862be2294d4d3631520bbdda
AryaTux <rahadian@users.noreply.github.com>
2017-04-10 12:21:35 +0700

    Update AntiSQLInjection.py

--- AntiSQLInjection.py
+++ AntiSQLInjection.py
@@ -1,26 +1,50 @@
-import time, os
+import time
+import os
 import sys
-import subprocess
+#import subprocess
 
 if not os.geteuid() == 0:
 	sys.exit ("You need root permission to run it !")
-os.system ('iptables -A INPUT -p tcp -s 192.168.1.0/24 -m string --string "%27" --algo bm -j LOG --log-prefix "SQL_INJECTION "')
-def follow(file):
-    file.seek(0,2)      
-    while True:
-         line = file.readline()
-         if not line:
-         		time.sleep(0.0)
-         		continue
-         yield line
+def opening():
+	print "===== AntiSQLInjection by AryaTux ====="
+	print "(1)Execute"
+	print "(2)Delete rules"
+	print "(3)Exit"
+	choose = raw_input ("What will you do ?")
+	choose = int(choose)
+	if choose == 1:
+		execute()
+	elif choose == 2:
+		delete()
+	elif choose == 3:
+		exit()
+	else:
+		print "Choose the correct number ! "
 
-logfile  = open("/var/log/kern.log")
-loglines = follow(logfile)
+def delete():
+	ipdel = raw_input("Enter your network ip(ex:192.168.0.0/24) :")
+	dibdel = "iptables -D INPUT -p tcp -s {} -m string --string \"%27\" --algo bm -j LOG --log-prefix \"SQL_INJECTION DETECTED\"" .format(ipdel)
+	os.system (dibdel)
+	print "DELETED !"
 
-for line in loglines:
-		print line,
-		if "SQL_INJECTION" in line:
-			os.system ('iptables -A INPUT -p tcp -s 192.168.1.0/24 -m string --string "%27" --algo bm -j REJECT')
-			print "DETECTED"
-
-
+def execute():
+	ipex = raw_input("Enter your network ip(ex:192.168.0.0/24) :")
+	dibex = "iptables -A INPUT -p tcp -s {} -m string --string \"%27\" --algo bm -j LOG --log-prefix \"SQL_INJECTION DETECTED\"" .format(ipex)
+	os.system (dibex)
+	def follow(file):
+    		file.seek(0,2)      # Go to the end of the file
+    		while True:
+			 	line = file.readline()
+				if not line:
+					time.sleep(0.0)
+       				continue
+    		yield line
+	logfile  = open("/var/log/kern.log")
+	loglines = follow(logfile)
+	for line in loglines:
+			print line,
+			if "SQL_INJECTION" in line:
+				dibex2 = "iptables -A INPUT -p tcp -s {} -m string --string \"%27\" --algo bm -j REJECT" .format(ipex)
+				os.system (dibex2)
+				print "DETECTED"
+opening()
