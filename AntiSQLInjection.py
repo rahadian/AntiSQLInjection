9473e51ca43804e255cf94419d1104f5ec99a1b7
AryaTux <rahadian@users.noreply.github.com>
2017-04-11 13:24:08 +0700

    Add files via upload

--- AntiSQLInjection.py
+++ AntiSQLInjection.py
@@ -14,7 +14,8 @@ def opening():
 	print colorred.format ("============================")
         print "(1)Execute"
         print "(2)Delete rules"
-        print "(3)Exit"
+        print "(3)Log"
+	print "(4)Exit"
         choose = raw_input ("What will you do ?")
         choose = int(choose)
         if choose == 1:
@@ -22,9 +23,10 @@ def opening():
         elif choose == 2:
                 delete()
         elif choose == 3:
+		log()
+        elif choose == 4:
 		print colorred.format("BYE !!!")
                 exit()
-        else:
                 print "Choose the correct number ! "
 
 def execute():
@@ -33,13 +35,13 @@ def execute():
         dibex2 = "iptables -A INPUT -p tcp -s {} -m string --string \"%27\" --algo bm -j REJECT" .format(ipex)
         os.system (dibex)
         def follow(file):
-                file.seek(0,2)      # Go to the end of the file
-                while True:
-                        line = file.readline()
-                        if not line:
-                                time.sleep(0.0)
-                                continue
-                        yield line
+               file.seek(0,2)      # Go to the end of the file
+               while True:
+                       line = file.readline()
+                       if not line:
+                               time.sleep(0.0)
+                               continue
+                       yield line
 
         logfile  = open("/var/log/kern.log")
 	loglines = follow(logfile)
@@ -48,8 +50,21 @@ def execute():
                 print line,
                 if "SQL_INJECTION DETECTED " in line:
                         os.system (dibex2)
-			exit()
+			return opening()
                         #print "DETECTED"
+def log():
+	def follow2(file):
+		#file.seek(0.2)
+		while True:
+			line2 = file.readline()
+			if not line2:
+				time.sleep(0.0)
+				continue
+			yield line2
+	logfile = open ("/var/log/kern.log")
+	loglines = follow2(logfile)
+	for line2 in loglines:
+		print line2,
 def delete():
         ipdel = raw_input("Enter your network ip(ex:192.168.0.0/24) :")
         dibdel1 = "iptables -D INPUT -p tcp -s {} -m string --string \"%27\" --algo bm -j LOG --log-prefix \"SQL_INJECTION DETECTED \"" .format(ipdel)
@@ -57,5 +72,5 @@ def delete():
         os.system (dibdel2)
 	os.system (dibdel1)
         print colorred.format ("DELETED !!!")
-
+	opening()
 opening()
